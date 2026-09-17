53a5d8cd4cdd9d4ee64cbfab4fedb025bfb98275
deardalyvn <huynhminhtruong2004@gmail.com>
2026-09-17 18:28:04 +0700

    Update Full Rect height from 170 to 240

--- waze-hud/main/display/layout.h
+++ waze-hud/main/display/layout.h
@@ -19,7 +19,7 @@ constexpr Rect Speed{85, 0, 80, 140};
 constexpr Rect Limits{165, 0, 60, 140};
 constexpr Rect Alerts{225, 0, 95, 140};
 constexpr Rect Street{0, 140, 320, 30};
-constexpr Rect Full{0, 0, 320, 170};
+constexpr Rect Full{0, 0, 320, 240};
 constexpr int MaxRegionPixels = Alerts.width * Alerts.height;
 }  // namespace layout
 
